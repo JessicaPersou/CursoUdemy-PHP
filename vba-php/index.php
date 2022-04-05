41c8f7b8ae565627e8c76f147eeb823de1ab07ba
Jessica Sousa <jessica.persou@gmail.com>
2022-04-05 17:17:17 -0300

    tentando ler arquivo

---
+++ vba-php/index.php
@@ -0,0 +1,16 @@
+<h1>Conversor do VBA</h1>
+
+<?php
+function jornada(){
+
+$caminho="";
+$validade="";
+$quantidade="";
+$validaDados="";
+
+if (empty($validaDados)){
+ include_once __DIR__ . "/../php-basic/var.php";
+}
+
+
+}
