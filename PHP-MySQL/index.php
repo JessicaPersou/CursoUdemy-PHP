1d4d84b3703ed35521746791d6ebed5a78330fd1
Jessica P Sousa <jessica.persou@gmail.com>
2022-04-19 00:12:25 -0300

    criando e deletando table

--- PHP-MySQL/index.php
+++ PHP-MySQL/index.php
@@ -15,11 +15,27 @@ if($conn->connect_errno){
     echo "Erro: ". mysqli_connect_error(); //mostrar o erro aqui 
 }
 
-//acessar e criar uma query
-$sql = " SELECT * FROM pessoas";
+//acessar uma query
+//$sql = " SELECT * FROM pessoas";
 
-$res = $conn->query($sql);
+//$res = $conn->query($sql);
 
-print_r($res);
+//print_r($res);
 
-$conn->close(); //finaliza o acesso ao banco de dados
+//$conn->close();  //finaliza o acesso ou conexão ao banco de dados
+
+
+
+//criando uma tabela
+$create = "CREATE TABLE teste (nome VARCHAR(100), sobrenome VARCHAR(100))";
+
+
+//para dropar executa o comando a seguir e comenta o comando acima 
+//$create = "DROP TABLE teste";
+
+// chamando a query para criar
+$conn->query($create); 
+
+$conn->close(); // sempre lembrar de fechar a função
+
+
