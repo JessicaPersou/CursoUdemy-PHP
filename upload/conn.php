3717cc2e7405f1f53262777c176606663c07f852
Jessica P Sousa <jessica.persou@gmail.com>
2022-04-26 23:00:16 -0300

    editando o front

--- upload/conn.php
+++ upload/conn.php
@@ -7,9 +7,6 @@ $db="upload";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
-if($conn->connect_errno){
+if($conn->connect_errno)
     exit("Falha na conexão com o banco de dados!");
-}else{
-echo "deu bom";
-}
 
--- upload/index.php
+++ upload/index.php
@@ -1,51 +1,79 @@
 <?php
 
-if(isset($_FILES['arquivo'])){
+include("conn.php");
+
+//if(isset($_FILES()))
+
+if (isset($_FILES['arquivo'])) {
     $arquivo = $_FILES['arquivo'];
 
-    if($arquivo['error'])
+    if ($arquivo['error'])
         die("Falha no envio do arquivo!");
-    
-        if($arquivo['size']>2097152)
-            die("Arquivo muito grande, Max: 2MB");
 
-            $pasta = "arquivos/";
-            $tmp = $arquivo['name'];
-            $fName = uniqid();
-            $extensao = strtolower(pathinfo($tmp, PATHINFO_EXTENSION));
+    if ($arquivo['size'] > 2097152)
+        die("Arquivo muito grande, Max: 2MB");
 
-            if($extensao != "jpg" && $extensao != "png")
-                die("Tipo de arquivo diferente, verifique a extensão");
-              
-    $path = $pasta . $fName . "." . $extensao;  
+    $pasta = "arquivos/";
+    $nomeArquivo = $arquivo['name'];
+    $novoNome = uniqid();
+    $extensao = strtolower(pathinfo($nomeArquivo, PATHINFO_EXTENSION));
+
+    if ($extensao != "jpg" && $extensao != "png")
+        die("Tipo de arquivo diferente, verifique a extensão.");
+
+    $path = $pasta . $novoNome . "." . $extensao;
     $ok = move_uploaded_file($arquivo["tmp_name"], $path);
 
-    if($ok){
-        $conn->query("INSERT INTO arquivos (nome , path) VALUES ('$tmp', '$path')") or die($conn->error);
-            echo "Arquivo enviado com sucesso.";
-        // Clique para <a target=\"_blank\" href=\"arquivos/$fName.$extensao\">Abrir!</a>";
-    }else
-        echo "Falha, tente novamente";
-    
+    if ($ok) {
+        $conn->query("INSERT INTO arquivos (nome , path, data_upload) VALUES ('$nomeArquivo', '$path', NOW())") or die($conn->error);
+
+        echo "Arquivo enviado com sucesso.";
+        // Clique para <a target=\"_blank\" href=\"arquivos/$nomeArquivo.$extensao\">Abrir!</a>";
+    } else
+        echo "Falha, tente novamente.";
 }
 
+$sql_query = $conn->query("SELECT * FROM arquivos") or die($conn->$mysqli->error);
 
 ?>
 
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Upload</title>
 </head>
+
 <body>
     <form method="POST" enctype="multipart/form-data" action="">
         <label for="">Selecione o Arquivo</label>
         <input name="arquivo" type="file" id="">
         <button name="upload" type="submit">Enviar Arquivo</button>
     </form>
+    <h1>Arquivos Enviados:</h1>
+    <table border="1" cellpadding="10">
+        <thead>
+            <th>PREVIEW</th>
+            <th>ARQUIVO</th>
+            <th>DATA DE ENVIO</th>
+        </thead>
+        <tbody>
+            <?php
+                while($arquivo = $sql_query->fetch_assoc()){
+            ?> 
+            <tr>
+                <td><img height="50" src="<?php echo $arquivo['path'];?>"</td>
+                <td><a target="_blank" href="<?php echo $arquivo['path'];?>"><?php echo $arquivo['nome'];?></a></td>
+                <td><?php echo date("d/m/Y H:i", strtotime($arquivo['data_upload']))?></td>
+            </tr>          
+            <?php    }?>
+        </tbody>
+    </table>
+
+
 </body>
+
 </html>
